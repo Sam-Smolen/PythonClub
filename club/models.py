37994ab3bf30caee22d72292ecad0b4580e30d79
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2022-02-03 14:49:15 -0800

    Test

--- club/models.py
+++ club/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.user
 
-# Create your models here.
+class Meeting(models.Models):
+    meetingtitle=models.CharField(max_length=255)
+    meetingdate=models.DateField()
+    
+
+    def__str__(self):
+        return self.meetingtitle
+
+    class Meta:
+        db_table='Meeting'
