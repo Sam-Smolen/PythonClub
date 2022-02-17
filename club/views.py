12c6b2306bd4fba617b593ff3ce704f6feaf6a2e
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2022-02-16 18:16:40 -0800

    Added meetings and meeting details

--- club/views.py
+++ club/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Resource, Meeting, MeetingMinutes, Event 
 
 # Create your views here.
@@ -8,3 +8,12 @@ def index(request):
 def resources(request):
     resource_list=Resource.objects.all()
     return render(request, 'club/resources.html' ,{'resource_list': resource_list})
+
+def meetings(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'club/meetings.html' ,{'meeting_list': meeting_list})
+
+def meetingdetails(request, id):
+    meeting=get_object_or_404(Meeting, pk=id)
+    return render(request, 'club/meetingdetails.html' ,{'meeting': meeting})
+	
